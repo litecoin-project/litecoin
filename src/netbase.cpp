6c1e196f02a93e3326a3cfb3b932bfd05689428c
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2026-05-25 20:36:25 -0500

    net: use single-result LookupHost in subnet lookup
    Ports Bitcoin commit a989f98d24.

--- src/netbase.cpp
+++ src/netbase.cpp
@@ -834,14 +834,11 @@ bool LookupSubNet(const std::string& strSubnet, CSubNet& ret)
         return false;
     }
     size_t slash = strSubnet.find_last_of('/');
-    std::vector<CNetAddr> vIP;
+    CNetAddr network;
 
     std::string strAddress = strSubnet.substr(0, slash);
-    // TODO: Use LookupHost(const std::string&, CNetAddr&, bool) instead to just get
-    //       one CNetAddr.
-    if (LookupHost(strAddress, vIP, 1, false))
+    if (LookupHost(strAddress, network, false))
     {
-        CNetAddr network = vIP[0];
         if (slash != strSubnet.npos)
         {
             std::string strNetmask = strSubnet.substr(slash + 1);
@@ -853,14 +850,15 @@ bool LookupSubNet(const std::string& strSubnet, CSubNet& ret)
             }
             else // If not a valid number, try full netmask syntax
             {
+                CNetAddr netmask;
                 // Never allow lookup for netmask
-                if (LookupHost(strNetmask, vIP, 1, false)) {
-                    ret = CSubNet(network, vIP[0]);
+                if (LookupHost(strNetmask, netmask, false)) {
+                    ret = CSubNet(network, netmask);
                     return ret.IsValid();
                 }
             }
         }
-        else
+        else // Single IP subnet (<ipv4>/32 or <ipv6>/128)
         {
             ret = CSubNet(network);
             return ret.IsValid();
